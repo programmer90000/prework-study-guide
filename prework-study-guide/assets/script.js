cd0ac3d391f1c5dcb7dfef518c0897f85e490146
Abdul Rehman <abdulr568programming@outlook.com>
2023-09-14 10:09:35 +0100

    If statements and loops

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,20 @@
+/*
+var topic = "HTML";
+
+if (topic === "HTML") {
+    console.log("Lets's study HTML");
+} else if (topic === "CSS") {
+    console.log("Let's study CSS");
+} else if (topic = "Git") {
+    console.log("Let's studt Git");
+} else if (topic = "JavaScript") {
+    console.log("Let's study JavaScript");
+} else {
+    console.log("Please try again");
+}
+*/
+var topics = ["HTML", "CSS", "Git", "JavaScript"];
+
+for (let i = 0; i < topics.length; i++) {
+    console.log(topics[i]);
+}
