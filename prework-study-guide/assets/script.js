e7edba0c92d38061bd70ce8b0d312832c4cf2845
Abdul Rehman <abdulr568programming@outlook.com>
2023-09-14 10:39:40 +0100

    If statements and loops

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -1,20 +1,30 @@
-/*
-var topic = "HTML";
-
-if (topic === "HTML") {
-    console.log("Lets's study HTML");
-} else if (topic === "CSS") {
-    console.log("Let's study CSS");
-} else if (topic = "Git") {
-    console.log("Let's studt Git");
-} else if (topic = "JavaScript") {
-    console.log("Let's study JavaScript");
-} else {
-    console.log("Please try again");
-}
-*/
 var topics = ["HTML", "CSS", "Git", "JavaScript"];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for (let i = 0; i < topics.length; i++) {
-    console.log(topics[i]);
+function listTopics() {
+    for (let i = 0; i < topics.length; i++) {
+        console.log(topics[i]);
+    }
 }
+
+function selectTopic() {
+    if (randomTopic === "HTML") {
+        console.log("Lets's study HTML");
+    } else if (randomTopic === "CSS") {
+        console.log("Let's study CSS");
+    } else if (randomTopic = "Git") {
+        console.log("Let's study Git");
+    } else if (randomTopic = "JavaScript") {
+        console.log("Let's study JavaScript");
+    } else {
+        console.log("Please try again");
+    }
+}
+
+console.log("Here are the topics we learned through Prework:");
+listTopics();
+
+console.log("\n")
+
+console.log("Which topic should we study first?")
+selectTopic();
